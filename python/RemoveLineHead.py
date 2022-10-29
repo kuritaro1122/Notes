3acc4a07bfe3f609151c52d427d22281667d713d
くり太郎 <62167170+kuritaro1122@users.noreply.github.com>
2022-10-29 12:46:46 +0900

    Update and rename RemoveLineHead.py to python/RemoveLineHead.py

--- python/RemoveLineHead.py
+++ python/RemoveLineHead.py
@@ -1,19 +1,15 @@
-path = str(input('path:'))
-rebase = bool(input('上書き(True, False):'))
-delimiter = str(input('区切り文字:'))
-
 lines = []
-a = 0
+path = str(input('path:'))
 
 with open(path, mode='r') as f:
     lines = f.readlines()
+    delimiter = str(input('delimiter:'))
     for index, line in enumerate(lines):
         texts = line.split(delimiter, 1)
-        print(texts)
         if len(texts) > 1:
             lines[index] = texts[1]
-    
 
 with open(path, mode='w') as f:
+    rebase = bool(input('rebase(True, False):'))
     if rebase:
         f.writelines(lines)
